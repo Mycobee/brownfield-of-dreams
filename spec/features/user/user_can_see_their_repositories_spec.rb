b2f8287ea0c88f9d1478794b879aaa62da4240f4
davisc0801 <davisc1014@gmail.com>
2019-07-02 13:32:09 -0600

    Clean using Rubocop

---
+++ spec/features/user/user_can_see_their_repositories_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'User' do
+  describe 'As a logged in user when I visit /dashboard' do
+    it "should have a section for 'Github'" do
+    end
+  end
+end
+# As a logged in user
+# When I visit /dashboard
+# Then I should see a section for "Github"
+# And under that section I should see a list of 5 repositories with the name of
+# each Repo linking to the repo on Github
