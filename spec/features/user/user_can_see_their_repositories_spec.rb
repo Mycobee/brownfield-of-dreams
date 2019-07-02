317da629d4033338e4ed4785b8e67b74905a7209
davisc0801 <davisc1014@gmail.com>
2019-07-02 13:56:39 -0600

    Add feature test to view a Github section.

--- spec/features/user/user_can_see_their_repositories_spec.rb
+++ spec/features/user/user_can_see_their_repositories_spec.rb
@@ -4,12 +4,13 @@ require 'rails_helper'
 
 describe 'User' do
   describe 'As a logged in user when I visit /dashboard' do
+    before :each do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
     it "should have a section for 'Github'" do
+      visit dashboard_path
+      expect(page).to have_all_of_selectors(".github_section")
     end
   end
 end
-# As a logged in user
-# When I visit /dashboard
-# Then I should see a section for "Github"
-# And under that section I should see a list of 5 repositories with the name of
-# each Repo linking to the repo on Github
