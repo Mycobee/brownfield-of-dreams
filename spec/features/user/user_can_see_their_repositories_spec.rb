297c53a96e07f04301859103165076974b558168
davisc0801 <davisc1014@gmail.com>
2019-07-02 17:39:01 -0600

    Refactor existing specs to use VCR

--- spec/features/user/user_can_see_their_repositories_spec.rb
+++ spec/features/user/user_can_see_their_repositories_spec.rb
@@ -9,8 +9,19 @@ describe 'User' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     end
     it "should have a section for 'Github'" do
-      visit dashboard_path
-      expect(page).to have_all_of_selectors(".github_section")
+      VCR.use_cassette("github_repositories") do
+        visit dashboard_path
+        expect(page).to have_all_of_selectors(".github_section")
+      end
+    end
+    it "shows a list of 5 repositories as links to each repository" do
+      VCR.use_cassette("github_repositories") do
+        visit dashboard_path
+        expect(page).to have_link("book_club")
+        expect(page).to have_link("1903_final")
+        expect(page).to have_link("activerecord-obstacle-course")
+        expect(page).to have_link("backend_prework")
+      end
     end
   end
 end
