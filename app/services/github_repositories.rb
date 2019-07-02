3b1fc88b776cfc6e13bc33876bdc8e7d2a2b690f
davisc0801 <davisc1014@gmail.com>
2019-07-02 17:40:28 -0600

    Add github repository service

---
+++ app/services/github_repositories.rb
@@ -0,0 +1,17 @@
+class GithubRepositories
+  def initialize(token)
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = "token " + token
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def find_repositories
+    repos = get_url("/user/repos")
+  end
+
+  def get_url(url)
+    response = @conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
