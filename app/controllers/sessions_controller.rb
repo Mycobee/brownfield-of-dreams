7efff7fdef0eb22a02bc673da6d7f915f07b2a0c
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-06 18:05:16 -0600

    Completed handrolled oauth for solo spike

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,48 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to dashboard_path
-    else
-      flash[:error] = "Looks like your email or password is invalid"
-      render :new
-    end
+		if !current_user.nil?
+			client_id     = "586cd4146e2b7b6b2621"
+  		client_secret = "6ba44bead24a403a61b05dbc84327e98368ca526"
+  		code          = params[:code]
+  		response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+			pairs = response.body.split("&")
+
+			response_hash = {}
+
+    	pairs.each do |pair|
+    		key, value = pair.split("=")
+    		response_hash[key] = value
+ 			end
+
+	  	token = response_hash["access_token"]
+
+			oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+			auth = JSON.parse(oauth_response.body)
+
+			if current_user.token.nil? || current_user.token.github_token.nil?
+		 		current_user.token = Token.create(github_token: token) 
+			elsif current_user.token.github_token != token 
+				current_user.token.update(github_token: token)
+			end
+			redirect_to dashboard_path
+		end
+
+		if current_user.nil?
+    	user = User.find_by(email: params[:session][:email]) || current_user
+    	if user && user.authenticate(params[:session][:password])
+    	  session[:user_id] = user.id
+    	  redirect_to dashboard_path
+    	else
+    	  flash[:error] = "Looks like your email or password is invalid"
+    	  render :new
+    	end
+		end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
   end
-
 end
--- app/facades/user_dashboard_facade.rb
+++ app/facades/user_dashboard_facade.rb
@@ -6,10 +6,12 @@ class UserDashboardFacade
   def repos(limit)
 		return nil if @token.nil? || @token.github_token.nil?
     repos = github_service.find_repositories
-    repo_objects = repos.map do |repo|
-      Repository.new(repo)
-    end
-    repo_objects.take(limit)
+
+   	repo_objects = repos.map do |repo|
+    	  Repository.new(repo)
+   	end
+
+   	repo_objects.take(limit)
   end
 
   private
