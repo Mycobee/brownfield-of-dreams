b31b24103e75e754eb2394ae0958e8857f27526a
davisc0801 <davisc1014@gmail.com>
2019-07-08 16:22:50 -0600

    Add update action to controller

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,10 +2,11 @@ class GithubController < ApplicationController
 	def create
 		token	= auth_hash['credentials']['token']
 		current_user.token = Token.find_or_create_by(github_token: token)
+		current_user.update(html_url: auth_hash[:extra][:raw_info][:html_url])
 		redirect_to dashboard_path
 	end
 
-	private 
+	private
 
 	def auth_hash
 		request.env['omniauth.auth']
