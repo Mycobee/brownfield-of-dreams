e76f1a415584f69fc50d0bc8b5bae3439a8f8b2d
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-09 16:14:06 -0600

    Adds add_default_video method for tutorial class

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -12,5 +12,12 @@ RSpec.describe Tutorial, type: :model do
 			tutorial = create(:tutorial)
 			expect(tutorial.no_videos?).to eq(true)
 		end
+
+		it 'add_default_video' do
+			tutorial = create(:tutorial)
+			tutorial.add_default_video
+			expect(tutorial.videos.first.title).to eq("Tutorial Has No Videos")
+			expect(tutorial.videos.first.description).to eq("")
+		end
 	end
 end
