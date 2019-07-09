d7340b2c64242734fc7ca5fc993eae472f512a98
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-08 20:13:48 -0600

    Fixed visitor_sees_homepage_spec rubocop offenses

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -6,16 +6,11 @@ describe 'Visitor' do
   describe 'on the home page' do
     it 'can see a list of tutorials' do
       tutorial1 = create(:tutorial)
-      tutorial2 = create(:tutorial)
-
-      video1 = create(:video, tutorial_id: tutorial1.id)
-      video2 = create(:video, tutorial_id: tutorial1.id)
-      video3 = create(:video, tutorial_id: tutorial2.id)
-      video4 = create(:video, tutorial_id: tutorial2.id)
+      create_list(:tutorial, 2)
 
       visit root_path
 
-      expect(page).to have_css('.tutorial', count: 2)
+      expect(page).to have_css('.tutorial', count: 3)
 
       within(first('.tutorials')) do
         expect(page).to have_css('.tutorial')
