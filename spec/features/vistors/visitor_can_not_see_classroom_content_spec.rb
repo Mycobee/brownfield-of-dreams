ce5d9ef9d749f0ca3702d56647f3ccad7708a5bd
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-09 13:54:40 -0600

    Adds invisible classroom content for visitors spec.

--- spec/features/vistors/visitor_can_not_see_classroom_content_spec.rb
+++ spec/features/vistors/visitor_can_not_see_classroom_content_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 feature "As a visitor on the root path" do
 	scenario "I shouldn't be able to see classroom content" do
-		
-		visit tutorials_path		
+		tutorial_1 = create(:tutorial, classroom: false)		
+		tutorial_2 = create(:tutorial, classroom: true)		
+		visit root_path		
+
+		expect(page).to have_link(tutorial_1.name)
+		expect(page).to_not have_link(tutorial_2.name)
 	end
 end
