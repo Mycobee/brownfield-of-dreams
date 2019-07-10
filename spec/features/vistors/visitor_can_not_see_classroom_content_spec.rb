5e278aa4a4fe5a8e2db1d6529e3c0e0a71ef9ae6
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-09 18:33:23 -0600

    Adds more paths to classroom content spec

--- spec/features/vistors/visitor_can_not_see_classroom_content_spec.rb
+++ spec/features/vistors/visitor_can_not_see_classroom_content_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 feature "As a visitor on the root path" do
-	scenario "I shouldn't be able to see classroom content" do
+	scenario "I can't see classroom content on tutorial or welcome paths" do
 		tutorial_1 = create(:tutorial, classroom: false)		
 		tutorial_2 = create(:tutorial, classroom: true)		
 		visit root_path		
 
+		expect(page).to have_link(tutorial_1.title)
+		expect(page).to_not have_link(tutorial_2.title)
+
+		visit tutorial_path(tutorial_2)
+
+		expect(response.status).to eq(404)
+		
+		visit tutorial_path(tutorial_1)
+		expect(page).to have_content(tutorial_1.title)
+
+		visit tutorials_path
 		expect(page).to have_link(tutorial_1.title)
 		expect(page).to_not have_link(tutorial_2.title)
 	end
