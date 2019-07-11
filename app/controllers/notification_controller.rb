b3a469a4aa0f80f1d0da0e74e2f7f2f08269815a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-11 17:56:09 -0600

    Added mailer call in users controller

--- app/controllers/notification_controller.rb
+++
@@ -1,7 +0,0 @@
-class NotificationController < ApplicationController
-	def create
-		RegistrationNotifierMailer.inform(current_user, params[:email]).deliver_now
-    flash[:notice] = "Your activation email has been sent"
-    redirect_to root_url
-	end
-end
