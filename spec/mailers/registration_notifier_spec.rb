845df8b72308e9358a2605ea5ef6f93a88c29167
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-12 21:02:18 -0600

    Adds email test spec

--- spec/mailers/registration_notifier_spec.rb
+++ spec/mailers/registration_notifier_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe RegistrationNotifierMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "Sends a registration email" do
+		user = create(:user, email: "1234@ideclarethumbwar.com")
+		email = RegistrationNotifierMailer.inform(user)
+		
+		expect(email.to.first).to eq(user.email)
+		expect(email.subject).to eq("Account Activation")
+		expect(email.body.encoded).to match(user.id.to_s)
+	end
 end
