e9fc2092b05225995fcae62bd1ae2e75db046958
davisc0801 <davisc1014@gmail.com>
2019-07-13 11:18:19 -0600

    Add tests for sending an invite

--- spec/features/user/user_can_send_an_invite_spec.rb
+++ spec/features/user/user_can_send_an_invite_spec.rb
@@ -4,6 +4,9 @@ describe "as a registed user" do
   describe "On / dashboard when I click on send an invite" do
     before :each do
       user = create(:user)
+      user.token = Token.new(github_token: ENV['GITHUB_API_KEY'])
+      user_2 = create(:user)
+      user.token = Token.new(github_token: ENV['GITHUB_API_KEY_2'])
       allow_any_instance_of(ApplicationController).to \
       receive(:current_user).and_return(user)
     end
@@ -11,6 +14,22 @@ describe "as a registed user" do
       visit dashboard_path
       click_link("Send an Invite")
       expect(current_path).to eq(invite_index_path)
+    end
+  end
+  describe "On /invite" do
+    it "allows us to send an invite" do
+      fill_in("Github Handle", with: "Mycobee")
+      click_button "Send Invite"
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Successfully sent invite!")
+    end
+  end
+  describe "Invite sad path" do
+    it "does not allow us to send an invite" do
+      fill_in("Github Handle", with: "WHomer")
+      click_button "Send Invite"
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Successfully sent invite!")
     end
   end
 end
