31c3fd91fffe70bb65fc461a63cc33566ef168d5
davisc0801 <davisc1014@gmail.com>
2019-07-13 15:04:29 -0600

    Add sendgrid setup to production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-	config.action_mailer.default_url_options = { :host => "localhost:3000" }	
+	config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -87,6 +87,18 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'brownfieldofdreams.herokuapp.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
