013af7a9d826baeef35347e99dd8bbcb8c934c48
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-13 14:32:40 -0600

    Cleans up params variable for readability

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,11 @@
 # Welcome controller for non users
 class WelcomeController < ApplicationController
   def index
+    page = params[:page]
     @tutorials = if params[:tag]
-                   Tutorial.non_classroom.paginate(page: params[:page], per_page: 5)
+                   Tutorial.non_classroom.paginate(page: page, per_page: 5)
                  else
-                   Tutorial.non_classroom.paginate(page: params[:page])
+                   Tutorial.non_classroom.paginate(page: page)
                  end
   end
 end
