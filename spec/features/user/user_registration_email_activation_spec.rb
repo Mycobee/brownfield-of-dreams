c81337dca47cea57fd17457730ecf5dc094475a6
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-07-12 21:01:55 -0600

    Formats user registration spec

--- spec/features/user/user_registration_email_activation_spec.rb
+++ spec/features/user/user_registration_email_activation_spec.rb
@@ -31,16 +31,3 @@ describe "As a guest on the registration path who fills in all fields" do
 		end	
 	end
 end
-
-#As a guest user
-#When I visit "/"
-#And I click "Register"
-#Then I should be on "/register"
-#And when I fill in an email address (required)
-#And I fill in first name (required)
-#And I fill in first name (required)
-#And I fill in password and password confirmation (required)
-#And I click submit
-#Then I should be redirected to "/dashboard"
-#And I should see a message that says "Logged in as <SOME_NAME>"
-#And I should see a message that says "This account has not yet been activated. Please check your email."
